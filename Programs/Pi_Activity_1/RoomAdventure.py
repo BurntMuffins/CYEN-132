6da5617449f00d173543ac1c89f34619abf603ec
BurntMuffins <aidanjs1208@gmail.com>
2023-03-29 15:06:50 -0500

    Added the inclass code

--- Programs/Pi_Activity_1/RoomAdventure.py
+++ Programs/Pi_Activity_1/RoomAdventure.py
@@ -114,16 +114,105 @@ class Game(Frame):
 
 	# creates the rooms
 	def createRooms(self):
-		pass
+		r1 = Room("Room 1", "images/room1.gif") 
+		r2 = Room("Room 2", "images/room2.gif") 
+		r3 = Room("Room 3", "images/room3.gif") 
+		r4 = Room("Room 4", "images/room4.gif") 
+
+		# exits for r1
+		r1.addExit("east", r2)
+		r1.addExit("south", r3)
+
+		# grabbables for r1
+		r1.addGrabbable("key")
+		
+		# items for r1
+		r1.addItem("chair", "It is made of wicker and no one is sitting on it.")
+		r1.addItem("table", "It is made of oak. A golden key rests on it.")
+
+		# add exits for r2
+		r2.addExit("west", r1)
+		r2.addExit("south" ,r4)
+
+		# add items to r2
+		r2.addItem("rug", "It is nice and Indian. It also needs to be vacuumed.")
+		r2.addItem("fireplace", "It is full of ashes.")
+
+		# add exits to r3
+		r3.addExit("north", r1)
+		r3.addExit("east", r4)
+
+		# add grabbables to r3
+		r3.addGrabbable("book")
+
+		# add items to r3
+		r3.addItem("bookshelves", "They are empty. Go figure.")
+		r3.addItem("statue", "There is nothing special about it.")
+		r3.addItem("desk", "The statue is resting on it. So is a book.")
+		
+
+		# add exits to r4
+		r4.addExit("north", r2)
+		r4.addExit("west", r3)
+		r4.addExit("south", None) # DEATH
+
+		# add grabbables to r4
+		r4.addGrabbable("6-pack")
+		r4.addItem("brew_rig", "Gourd is brewing some sort of oatmeal stout on \
+			the brew rig. A 6-pack is resting beside it.")
+		
+		# set r1 as the current at the beginning of the game
+		Game.currentRoom = r1
+		Game.inventory = []
+
+
 	# sets up the GUI
 	def setupGUI(self):
-		pass
+		self.pack(fill=BOTH, expand=1)
+		
+		# user input stuff
+		Game.player_input = Entry(self, bg="white")
+		Game.player_input.bind("<Return>", self.process)
+		Game.player_input.pack(side=BOTTOM, fill=X)
+		Game.player_input.focus()
+
+		# image on left
+		img = None
+		Game.image = Label(self, width=WIDTH//2, image=img)
+		Game.image.image = img
+		Game.image.pack(side=LEFT, fill=Y)
+		Game.image.pack_propagate(False) # doesn't allow widget to determine size
+
+		# text information on right
+		text_frame = Frame(self, width=WIDTH//2)
+		Game.text = Text(text_frame, bg="lightgrey", state=DISABLED)
+		Game.text.pack(fill=Y, expand=1)
+		text_frame.pack(side=RIGHT, fill=Y)
+		text_frame.pack_propagate(False) # doesn't allow widget to determine size
+
 	# sets the current room image
 	def setRoomImage(self):
-		pass
+		if Game.currentRoom == None:
+			Game.img = PhotoImage(file="images/skull.gif")
+		else:
+			Game.img = PhotoImage(file=Game.currentRoom.image)
+		
+		# displaying the image on the left
+		Game.image.config(image=Game.img)
+		Game.image.image = Game.img
+
 	# sets the status displayed on the right of the GUI
 	def setStatus(self, status):
-		pass
+		Game.text.config(state=NORMAL)
+		Game.text.delete("1.0", END)
+		if (Game.currentRoom == None):
+			# dead state
+			Game.text.insert(END, "You are dead. The only thing you can do now is quit.\n")
+		else:
+			# display current room and other info
+			Game.text.insert(END, f"{Game.currentRoom}\nYou are carrying {Game.inventory}\n\n{status}")
+		Game.text.config(state=DISABLED)
+
 	# plays the game
 	def play(self):
 		# add the rooms to the game
@@ -137,7 +226,76 @@ class Game(Frame):
 
 	# processes the player's input
 	def process(self, event):
-		pass
+		# grab the player's input from the input at the bottom of the GUI
+		action = Game.player_input.get()
+		# set the user's input to lowercase to make it easier to compare the verb and noun to known values
+		action = action.lower()
+		# set a default response
+		response = "I don't understand.  Try verb noun.  Valid verbs are go, look, and take"
+
+		# exit the game if the player wants to leave (supports quit, exit, and bye)
+		if (action == "quit" or action == "exit" or action == "bye" or action == "sionara!"):
+			exit(0)
+
+		# if the player is dead if goes/went south from room 4
+		if (Game.currentRoom == None):
+			# clear the player's input
+			Game.player_input.delete(0, END)
+			return
+
+		# split the user input into words (words are separated by spaces) and store the words in a list
+		words = action.split()
+
+		# the game only understands two word inputs
+		if (len(words) == 2):
+			# isolate the verb and noun
+			verb = words[0]
+			noun = words[1]
+
+			# the verb is: go
+			if (verb == "go"):
+				# set a default response
+				response = "Invalid exit."
+
+				# check for valid exits in the current room
+				if (noun in Game.currentRoom.exits):
+					# if one is found, change the current room to the one that is associated with the specified exit
+					Game.currentRoom = Game.currentRoom.exits[noun]
+					# set the response (success)
+					response = "Room changed."
+			# the verb is: look
+			elif (verb == "look"):
+				# set a default response
+				response = "I don't see that item."
+
+				# check for valid items in the current room
+				if (noun in Game.currentRoom.items):
+					# if one is found, set the response to the item's description
+					response = Game.currentRoom.items[noun]
+			# the verb is: take
+			elif (verb == "take"):
+				# set a default response
+				response = "I don't see that item."
+
+				# check for valid grabbable items in the current room
+				for grabbable in Game.currentRoom.grabbables:
+					# a valid grabbable item is found
+					if (noun == grabbable):
+						# add the grabbable item to the player's inventory
+						Game.inventory.append(grabbable)
+						# remove the grabbable item from the room
+						Game.currentRoom.delGrabbable(grabbable)
+						# set the response (success)
+						response = "Item grabbed."
+						# no need to check any more grabbable items
+						break
+
+		# display the response on the right of the GUI
+		# display the room's image on the left of the GUI
+		# clear the player's input
+		self.setStatus(response)
+		self.setRoomImage()
+		Game.player_input.delete(0, END)
 
 ##########################################################
 # the default size of the GUI is 800x600
